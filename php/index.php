8fe74fedfde74162788e1266704a768bd2392b00
Prateek Mishra <75943911+Prateek692@users.noreply.github.com>
2021-11-25 03:27:25 +0530

    mk

--- php/index.php
+++ php/index.php
@@ -10,16 +10,179 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>My website</title>
-</head>
-<body>
-
-	<a href="logout.php">Logout</a>
-	<h1>This is the index page</h1>
-	<h1>MAI CHUTIYA HU JO YAHA AAYA	</h1>
-
-	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+    <head>
+        <meta name="viewport" content="with=device-width, initial-scale=1.0">
+        <title>Student Profile</title>
+        <link rel="icon" href="images/icon.jpg">
+        <link rel="stylesheet" href="student_profile.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    </head>
+<body onload="delay()">
+    <div id="loading">
+    </div>
+    <div class="logout">
+      <a href="php/logout.php"><button>Logout</button></a>
+    </div>
+    <h1 style="color: black; font-size: 40px; text-align: center;">Student Profile</h1>
+<!-- Student Profile -->
+<div class="student-profile">
+        <div class="card">
+            <div class="card-header">
+              <img class="profile_img" src="https://placeimg.com/640/480/arch/any" alt="">
+              <h3>Name of the Student</h3>
+            </div>
+            <div class="card-body">
+              <p class="mb-0"><strong class="pr-1">Roll No:</strong>321000001</p>
+              <p class="mb-0"><strong class="pr-1">Date of Birth:</strong>4</p>
+              <p class="mb-0"><strong class="pr-1">Gender:</strong>A</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+              <h3 class="mb-0"><i class="far fa-clone pr-1"></i>General Information</h3>
+            </div>
+            <div class="card-body pt-0">
+              <table class="table table-bordered">
+                <tr>
+                  <th width="30%">Degree</th>
+                  <td width="2%">:</td>
+                  <td>125</td>
+                </tr>
+                <tr>
+                  <th width="30%">Branch Name</th>
+                  <td width="2%">:</td>
+                  <td>2020</td>
+                </tr>
+                <tr>
+                  <th width="30%">Specialization</th>
+                  <td width="2%">:</td>
+                  <td>Group</td>
+                </tr>
+                <tr>
+                  <th width="30%">Section</th>
+                  <td width="2%">:</td>
+                  <td>B+</td>
+                </tr>
+                <tr>
+                  <th width="30%">Blood Group</th>
+                  <td width="2%">:</td>
+                  <td>Male</td>
+                </tr>
+                <tr>
+                  <th width="30%">Mobile No</th>
+                  <td width="2%">:</td>
+                  <td>Male</td>
+                </tr>
+                <tr>
+                  <th width="30%">Email Address</th>
+                  <td width="2%">:</td>
+                  <td>Male</td>
+                </tr>
+                <tr>
+                  <th width="30%">Residential Address</th>
+                  <td width="2%">:</td>
+                  <td>Male</td>
+                </tr>
+              </table>
+            </div>
+        </div>
+</div>
+<!-- Student Profile -->
+<div class="student-profile">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="mb-0"><i class="far fa-clone pr-1"></i>Course-Wise Attendance</h3>
+          </div>
+          <div class="card-body pt-0">
+            <table class="table table-bordered">
+              <tr>
+                <th width="30%">Web Technology</th>
+                <td width="2%">:</td>
+                <td>125</td>
+              </tr>
+              <tr>
+                <th width="30%">Computer Architecture and Organization</th>
+                <td width="2%">:</td>
+                <td>2020</td>
+              </tr>
+              <tr>
+                <th width="30%">Database Management System</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Design and Analysis of Algorithms</th>
+                <td width="2%">:</td>
+                <td>B+</td>
+              </tr>
+              <tr>
+                <th width="30%">Microprocessors and Microcontrollers</th>
+                <td width="2%">:</td>
+                <td>Male</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <h3 class="mb-0"><i class="far fa-clone pr-1"></i>Semester Wise GPA</h3>
+          </div>
+          <div class="card-body pt-0">
+            <table class="table table-bordered">
+              <tr>
+                <th width="30%">Semester 1</th>
+                <td width="2%">:</td>
+                <td>125</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 2</th>
+                <td width="2%">:</td>
+                <td>2020</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 3</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 4</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 5</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 6</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 7</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+              <tr>
+                <th width="30%">Semester 8</th>
+                <td width="2%">:</td>
+                <td>Group</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+</div>
 </body>
+    <script>/*Loader*/
+    var preloader=document.getElementById('loading');
+             function delay(){
+                 setInterval(myFunction,1500);
+             }
+             function myFunction(){
+                 preloader.style.display='none';
+             }
+    </script>
 </html>
--- php/login.php
+++ php/login.php
@@ -44,3 +44,99 @@ session_start();
 	}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="with=device-width, initial-scale=1.0">
+        <title>University Website</title>
+        <link rel="icon" href="/images/icon.jpg">
+        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body onload="delay()">
+        <div id="loading">
+        </div>
+    
+        <section class="sub-header">
+            <nav>
+                <a href="index.html"><img src="images/logo.png"></a>
+                <div class="nav-links" id="navLinks">
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+				<ul>
+					<li><a href="about.html">ABOUT</a></li>
+					<li><a href="faq.html">FAQs</a></li>
+					<li><a href="course.html">OUR COURSES</a></li>
+					<li><a href="notices.html">NOTICES</a></li>
+					<li><a href="contact.html">CONTACT US</a></li>
+					<li><a href="login.php">LOGIN</a></li>
+					<li><a href="locate.html"><i class="fa fa-map-marker" style="color: white; display: contents; font-size: 25px;"></i></a></li>
+				</ul>
+                </div>
+                <i class="fa fa-bars" onclick="showMenu()"></i>
+            </nav>
+        </section>
+    <div class="login-container" id="login-container">
+        <div class="form-container sign-up-container">
+            <form action="php/signup.php" method="post">
+                <h1>Create Account</h1>
+                <input type="text" placeholder="Username" id="text" name="user_name">
+                <input type="password" id="text" name="password" placeholder="Password" >
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="php/login.php" method="post">
+                <h1>Sign in</h1>
+                <input type="text" placeholder="Username" id="text" name="user_name">
+                <input type="password" id="text" name="password" placeholder="Password" >
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+	const signUpButton = document.getElementById('signUp');
+	const signInButton = document.getElementById('signIn');
+	const container = document.getElementById('login-container');
+	signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+	});
+	signInButton.addEventListener('click', () => {
+		container.classList.remove("right-panel-active");
+	});
+	</script>
+    <script>/*Loader*/
+	var preloader=document.getElementById('loading');
+             function delay(){
+                 setInterval(myFunction,1500);
+             }
+             function myFunction(){
+                 preloader.style.display='none';
+             }
+	</script>
+    <script> /*For menu in mobile devices*/
+		var navLinks = document.getElementById("navLinks");
+            function showMenu(){
+                navLinks.style.right = "0";
+            }
+            function hideMenu(){
+                navLinks.style.right = "-820px";
+            }
+	</script>
+    </body>
+</html>
