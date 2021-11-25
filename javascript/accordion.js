caeb0be003ca69a3708e283c148219eb67b1b9dc
Prateek Mishra <75943911+Prateek692@users.noreply.github.com>
2021-11-25 13:45:16 +0530

    faq

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,16 +1,19 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+var shows = document.querySelectorAll(".show");
+var infos = document.querySelectorAll(".info");
+var accordions = document.querySelectorAll(".accordion");
+
+for (let i = 0; i < shows.length; i++) {
+  shows[i].onclick = function() {
+    for (let j = 0; j < shows.length; j++) {
+      if (shows[j] != this) {
+        infos[j].classList.remove("reveal");
+        shows[j].innerHTML = "+";
+        accordions[j].classList.remove("radius");
+      }
     }
-  });
+    accordions[i].classList.toggle("radius");
+    infos[i].classList.toggle("reveal");
+    if (infos[i].className.includes("reveal")) shows[i].innerHTML = "-";
+    else shows[i].innerHTML = "+";
+  };
 }
